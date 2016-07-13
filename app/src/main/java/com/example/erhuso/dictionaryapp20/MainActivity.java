585624bbb846a4a549570df958eaabf55e742cf5
Eriks Work Computer <erhuso@gmail.com>
2016-07-13 11:06:02 -0700

    Changed text view to a list view, added an adaptor and finished the project

--- app/src/main/java/com/example/erhuso/dictionaryapp20/MainActivity.java
+++ app/src/main/java/com/example/erhuso/dictionaryapp20/MainActivity.java
@@ -6,15 +6,28 @@ import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //For the simplecursor adaptor to match user dictionary columns to layout items
+
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[] {
+            UserDictionary.Words.WORD,
+            UserDictionary.Words.FREQUENCY};
+
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[] {
+            android.R.id.text1,
+            android.R.id.text2
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         //start app
 
-        //create textview
-        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
+        //create List
+        ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
 
         //get content resolver which will send a message to the content provider
 
@@ -38,36 +51,46 @@ public class MainActivity extends AppCompatActivity {
 
         Log.e("Test", UserDictionary.Words.CONTENT_URI.toString());
 
-        try {
+        //set cursor adaptor to fill standard two_line_list_item with layout from the cursor
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cursor, COLUMNS_TO_BE_BOUND, LAYOUT_ITEMS_TO_FILL,0);
 
-            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words.\n");
-            dictTextView.append("Columns: " + UserDictionary.Words._ID + " - " + UserDictionary.Words.FREQUENCY + " - " + UserDictionary.Words.WORD);
+        //attach the adaptor to the list view
+        dictListView.setAdapter(cursorAdapter);
 
-
-            //get the index of the columns using dictionary.words
-
-            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
-            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY) ;
-            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
-
-            while (cursor.moveToNext())
-            {
-                //get data from columns
-                int id = cursor.getInt(idColumn);
-                int frequency = cursor.getInt(frequencyColumn);
-                String word = cursor.getString(wordColumn);
-
-                //append table
-                dictTextView.append("\n" + id + " - " + frequency + " - " + word);
-            }
-
-
-        } finally {
-
-            //close the cursor
-            cursor.close();
-
-        }
+//        try {
+//
+//            //get the index of the columns using dictionary.words
+//
+//            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+//            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY) ;
+//            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+//
+//
+//
+//            dictListView.setText("The UserDictionary contains " + cursor.getCount() + " words.\n");
+//            dictListView.append("Columns: " + UserDictionary.Words._ID + " - " + UserDictionary.Words.FREQUENCY + " - " + UserDictionary.Words.WORD);
+//
+//
+//
+//
+//            while (cursor.moveToNext())
+//            {
+//                //get data from columns
+//                int id = cursor.getInt(idColumn);
+//                int frequency = cursor.getInt(frequencyColumn);
+//                String word = cursor.getString(wordColumn);
+//
+//                //append table
+//                dictListView.append("\n" + id + " - " + frequency + " - " + word);
+//            }
+//
+//
+//        } finally {
+//
+//            //close the cursor
+//            cursor.close();
+//
+//        }
 
     }
 
